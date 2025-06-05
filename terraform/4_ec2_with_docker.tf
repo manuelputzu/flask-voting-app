f8491301705aea9769ae47c1f94f39f018c4893d
manuelputzu <manuel.putzu@gmx.de>
2025-06-05 15:19:52 +0200

    Terraform deployment fixes - duplicate outputs now unique within module

--- terraform/4_ec2_with_docker.tf
+++ terraform/4_ec2_with_docker.tf
@@ -32,13 +32,3 @@ resource "aws_instance" "web" {
     Name = "FlaskVotingApp"
   }
 } 
-
-output "web_public_ip" {
-  value = aws_instance.web.public_ip
-  description = "Public IP of the EC2 instance"
-}
-
-output "rds_endpoint" {
-  value = aws_db_instance.votes_db.endpoint
-  description = "Endpoint of the RDS instance"
-}
--- terraform/5_outputs.tf
+++ terraform/5_outputs.tf
@@ -1,8 +1,9 @@
-output "web_instance_public_ip" {
-  description = "Public IP of the EC2 instance running the Flask app"
+output "web_public_ip" {
   value = aws_instance.web.public_ip
+  description = "Public IP of the EC2 instance"
 }
 
 output "rds_endpoint" {
   value = aws_db_instance.votes_db.endpoint
+  description = "Endpoint of the RDS instance"
 }
