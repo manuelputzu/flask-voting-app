b14a7e3a83142f3b2306c0ecfd1968722d83c96e
manuelputzu <manuel.putzu@gmx.de>
2025-06-05 17:34:00 +0200

    Docker container auto-start via Terraform, .env fix, user_data cleanup

--- terraform/4_ec2_with_docker.tf
+++ terraform/4_ec2_with_docker.tf
@@ -7,26 +7,34 @@ resource "aws_instance" "web" {
   associate_public_ip_address = true
 
   user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install -y docker.io git
-              systemctl start docker
-              systemctl enable docker
+    #!/bin/bash
+    exec > /var/log/user-data.log 2>&1
+    set -x
 
-              cd /home/ubuntu
-              git clone https://github.com/manuelputzu/flask-voting-app.git
+    apt update -y
+    apt install -y docker.io git
 
-              cat <<EOT > /home/ubuntu/flask-voting-app/.env
-              DB_HOST=${aws_db_instance.votes_db.endpoint}
-              DB_PORT=3306
-              DB_NAME=votes_db
-              DB_USER=${var.db_username}
-              DB_PASS=${var.db_password}
-              EOT
+    systemctl start docker
+    systemctl enable docker
 
-              docker pull mputzu/flask-voting-app:latest
-              docker run -d --env-file /home/ubuntu/flask-voting-app/.env -p 5000:5000 mputzu/flask-voting-app:latest
-              EOF
+    mkdir -p /home/ubuntu
+    chown ubuntu:ubuntu /home/ubuntu
+    cd /home/ubuntu
+
+    git clone https://github.com/manuelputzu/flask-voting-app.git
+
+    # Create .env file with DB credentials
+    cat <<EOT > /home/ubuntu/flask-voting-app/.env
+    DB_HOST=${aws_db_instance.votes_db.endpoint}
+    DB_NAME=votes_db
+    DB_USER=${var.db_username}
+    DB_PASS=${var.db_password}
+    DB_PORT=3306
+    EOT
+
+    docker pull mputzu/flask-voting-app:latest
+    /usr/bin/docker run -d --env-file /home/ubuntu/flask-voting-app/.env -p 5000:5000 mputzu/flask-voting-app:latest
+  EOF
 
   tags = {
     Name = "FlaskVotingApp"
